16935654c96bfdd647fdd54048af336727f46c96
Nikita <robotomize@gmail.com>
2015-02-08 22:44:09 +1000

    Update parser.php

--- parser.php
+++ parser.php
@@ -1,8 +1,8 @@
 <?php
-
+include "db.php";
 include "simpledom/simple_html_dom.php";
 $firstArray[] = array();
-for($counter=0;$counter<10;$counter++)
+for($counter=0;$counter<800;$counter++)
 {
 $html = file_get_html("http://rabota.sakh.com/resume/?list=".$counter);
 for($i=0;$i<17;$i++)
@@ -51,7 +51,29 @@ foreach($html->find('nobr[class=green bold]') as $element)
         else { $j++; }
 
       }
+
+$data = array(null,$firstArray[$counter][$i]["0"],$firstArray[$counter][$i]["1"],$firstArray[$counter][$i]["2"],$firstArray[$counter][$i]["3"]);
+               try
+                {
+                    $STH = DBmodel::getInstance()->prepare("INSERT INTO main_data (id,name,type,about,salary) values (?,?,?,?,?)");
+                    $STH->execute($data);
+                }
+                catch(PDOException $e)
+                {
+                   // file_put_contents('/var/www/FeedBrother/PDOErrors.txt', "Ошибка в методе для счетчика просмотров".$e->getMessage(), FILE_APPEND);
+                }
+
+
+
 }
+//var_dump($firstArray);
+    //            }
+      //          catch(PDOException $e)
+        //        {
+                   // file_put_contents('/var/www/FeedBrother/PDOErrors.txt', "Ошибка в методе для счетчика просмотров".$e->getMessage(), FILE_APPEND);
+          //      }
+
+
 echo "Progress".round((($counter/800)*100))."%"."\n";
 }
 var_dump($firstArray);
