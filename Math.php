ab3c67118c3875e63acb3292e944ccc537845272
Nikita <robotomize@gmail.com>
2015-02-14 15:49:09 +1000

    Create Math.php

---
+++ Math.php
@@ -0,0 +1,25 @@
+<?php
+
+class Math
+{
+
+	static $avgValue = "";
+	static $maxValue = "";
+
+	public ProgressBarAvg(self::$avgValue)
+	{		
+		return round(round(25000/self::$avgValue)*100);
+	}
+	public ProgressBarMax(self::$maxValue)
+	{
+		return round(round(35000/self::$maxValue)*100);
+	}
+
+	 public function __construct($param1,$param2)
+        {
+        	self::$avgValue = $param1;
+        	self::$maxValue = $param2;
+        }
+
+
+}
