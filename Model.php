fedea194e894b075b958a96d0ac69ec9dd06fc95
Nikita <robotomize@gmail.com>
2015-02-14 17:12:38 +0900

    Update Model.php

--- Model.php
+++ Model.php
@@ -1,7 +1,7 @@
 <?php
 
 include "db.php";
-include "Math.php";
+include "math.php";
 /**
 *  // реализация простой модели
 */
@@ -29,12 +29,12 @@ class Model
         }
         public function getMaxSalaryByOld()
         {
-            $result = DBmodel::getInstance()->query("SELECT old, MAX(salary),name FROM main_data GROUP BY old");
+            $result = DBmodel::getInstance()->query("SELECT old, MAX(salary) FROM main_data GROUP BY old");
             return $result->fetchAll();
         }
         public function getAvgSalaryByOld()
         {
-        	 $result = DBmodel::getInstance()->query("SELECT old, AVG(salary),name FROM main_data GROUP BY old");
+        	 $result = DBmodel::getInstance()->query("SELECT old, AVG(salary) FROM main_data GROUP BY old");
            return $result->fetchAll();
         }
 
@@ -46,7 +46,7 @@ class Model
 
         public function getMaxSalryFromAll()
         {
-           $result = DBmodel::getInstance()->query("SELECT MAX(salary),name FROM main_data");
+           $result = DBmodel::getInstance()->query("SELECT MAX(salary) FROM main_data");
            return $result->fetch();
         }
 
@@ -58,6 +58,10 @@ class Model
 $objectStart = new Model();
 
 $dataAvgSalaryByType = $objectStart->getAvgSalaryByOld();
+$dataMaxSalaryByOld = $objectStart->getMaxSalaryByOld();
+
+$dataAvgSalaryFromType = $objectStart->getAvgSalary();
+$dataMaxSalaryFromType = $objectStart->getMaxSalaryByType();
 
 $dataAvgSalaryFromAll = $objectStart->getAvgSalryFromAll();
 $dataMaxSalaryFromAll = $objectStart->getMaxSalryFromAll();
@@ -102,9 +106,9 @@ $objectConvert = new Math(round($dataAvgSalaryFromAll["0"]),round($dataMaxSalary
                        2]);
 
       var options = {
-        title: "Density of Precious Metals, in g/cm^3",
-        width: 600,
-        height: 400,
+        title: "График зависимости средней зарплаты от возраста",
+         width: "100%",
+        height: "720",
         bar: {groupWidth: "95%"},
         legend: { position: "none" },
       };
@@ -112,6 +116,105 @@ $objectConvert = new Math(round($dataAvgSalaryFromAll["0"]),round($dataMaxSalary
       chart.draw(view, options);
   }
   </script>
+
+ <script type="text/javascript">
+
+      google.load("visualization", "1", {packages:["corechart"]});
+      google.setOnLoadCallback(drawChart);
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ["Возраст", "Зарплата", { role: "style" }],
+          <?php
+                $counter = 0;
+                while($counter<count($dataMaxSalaryByOld))
+                {
+                        $dataMaxSalaryByOld[$counter][1] = round($dataMaxSalaryByOld[$counter][1]);
+                        echo "['{$dataMaxSalaryByOld[$counter][0]}', {$dataMaxSalaryByOld[$counter][1]},'stroke-color: #871B47; stroke-opacity: 0.8; stroke-width: 8; fill-color: #BC5679; fill-opacity: 0.2'],";                      
+                        $counter++;
+                }
+          ?>
+        ]);
+
+       var view = new google.visualization.DataView(data);
+      view.setColumns([0, 1,
+                       { calc: "stringify",
+                         sourceColumn: 1,
+                         type: "string",
+                         role: "annotation" },
+                       2]);
+
+      var options = {
+        title: "График зависимости максимальной зарплаты от возраста",
+        width: "100%",
+        height: "720",
+        bar: {groupWidth: "95%"},
+        legend: { position: "none" },
+      };
+      var chart = new google.visualization.ColumnChart(document.getElementById("columnchart_values_max_old"));
+      chart.draw(view, options);
+  }
+  </script>
+
+<script type="text/javascript">
+      google.load("visualization", "1", {packages:["corechart"]});
+      google.setOnLoadCallback(drawChart);
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Тип деятельности', 'Зарплата'],
+           <?php
+                $counter = 0;
+                while($counter<count($dataAvgSalaryFromType))
+                {
+                        $dataAvgSalaryFromType[$counter][1] = round($dataAvgSalaryFromType[$counter][1]);
+                        echo "['{$dataAvgSalaryFromType[$counter][0]}', {$dataAvgSalaryFromType[$counter][1]}],";                      
+                        $counter++;
+                }
+          ?>
+        ]);
+
+        var options = {
+          title: 'Зависимость среднего значения зарплаты от вида деятельности',
+          pieHole: 0.4,
+            width: "100%",
+             height: "720",
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('donutchartAvgType'));
+        chart.draw(data, options);
+      }
+    </script>
+
+
+    <script type="text/javascript">
+      google.load("visualization", "1", {packages:["corechart"]});
+      google.setOnLoadCallback(drawChart);
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Тип деятельности', 'Зарплата'],
+           <?php
+                $counter = 0;
+                while($counter<count($dataMaxSalaryFromType))
+                {
+                        $dataMaxSalaryFromType[$counter][1] = round($dataMaxSalaryFromType[$counter][1]);
+                        echo "['{$dataMaxSalaryFromType[$counter][0]}', {$dataMaxSalaryFromType[$counter][1]}],";                      
+                        $counter++;
+                }
+          ?>
+        ]);
+
+        var options = {
+          title: 'Зависимость максимального значения зарплаты от вида деятельности',
+          pieHole: 0.4,
+            width: "100%",
+        height: "720",
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('donutchartMaxType'));
+        chart.draw(data, options);
+      }
+    </script>
+
+
   </head>
 <nav class="navbar navbar-default">
   <div class="container-fluid">
@@ -144,44 +247,58 @@ $objectConvert = new Math(round($dataAvgSalaryFromAll["0"]),round($dataMaxSalary
           </ul>
         </li>
       </ul>
-      <form class="navbar-form navbar-left" role="search">
-        <div class="form-group">
-          <input type="text" class="form-control" placeholder="Search">
-        </div>
-        <button type="submit" class="btn btn-default">Submit</button>
-      </form>
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="#">Link</a></li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
-          <ul class="dropdown-menu" role="menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li class="divider"></li>
-            <li><a href="#">Separated link</a></li>
-          </ul>
-        </li>
-      </ul>
+
     </div>
   </div>
 </nav>
 
+
+
+ 
+
+<div class="page-header">
+  <h1>&nbsp;&nbsp;Аналитика резюме с сайта <a href="http://sakh.com">Sakh.com</a> <small>Основная цель - аналитика зарплатных ожиданий</small></h1>
+</div>
 <div class="container-fluid">
-    <!-- на будущее прогресс бары -->
-    <div class="progress">
-      <div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="25000" aria-valuemin="0" aria-valuemax=<?php echo round($dataAvgSalaryFromAll["0"]); ?> style=<?php echo ProgressBarAvg(); ?>>
-        <span class="sr-only">Зарплата относительно средней</span>
+
+  <div class="row">
+  <div class="col-md-12">
+   <div id="columnchart_values"></div>
+
+  </div>
+  </div>
+<div class="row">
+    <div class="col-md-12">  
+   <div id="columnchart_values_max_old"></div>   
+    </div> 
+</div>
+<div class="row">
+<div class="col-md-12">
+<div id="donutchartAvgType"></div>
+</div>
+</div>
+
+<div class="row">
+<div class="col-md-12">
+<div id="donutchartMaxType"></div>
+</div>
+</div>
+
+
+       <div><strong>Зарплата относительно средней(<?php echo round($dataAvgSalaryFromAll["0"]); ?>)</strong></div><br>
+    <div class="progress">     
+      <div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="25000" aria-valuemin="0" aria-valuemax=<?php echo round($dataAvgSalaryFromAll["0"]); ?> style=<?php echo $objectConvert->ProgressBarAvg(); ?>>
+        
       </div>
     </div>
-
+    <!-- на будущее прогресс бары -->
+ 
+<div><strong>Зарплата относительно максимальной(<?php echo round($dataMaxSalaryFromAll["0"]); ?>)</strong></div><br>
     <div class="progress">
-        <div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="25000" aria-valuemin="0" aria-valuemax=<?php echo round($dataMaxSalaryFromAll["0"]); ?> style=<?php echo ProgressBarMax(); ?>>
-          <span class="sr-only">Зарплата относительно максимальной</span>
+        <div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="25000" aria-valuemin="0" aria-valuemax=<?php echo round($dataMaxSalaryFromAll["0"]); ?> style=<?php echo $objectConvert->ProgressBarMax(); ?>>
+        
         </div>
       </div>
-
-		<center><div id="columnchart_values"></div></center>  
 
 </div>
 
