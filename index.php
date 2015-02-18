a8dcce77c2887a3e6be5e66322a32610992c2ba0
Nikita <robotomize@gmail.com>
2015-02-18 20:37:06 +1000

    Update index.php

--- index.php
+++ index.php
@@ -23,18 +23,25 @@ $objectConvert = new Math(round($dataAvgSalaryFromAll["0"]),round($dataMaxSalary
   <head>
 
         <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Аналитика резюме с http://sakh.com</title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
         <link href='http://fonts.googleapis.com/css?family=Open+Sans:700' rel='stylesheet' type='text/css'>
         <link href="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.0-beta.3/css/select2.min.css" />
-
+        <link href='http://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'> 
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:300' rel='stylesheet' type='text/css'>     
+        <link href="assets/font-awesome-4.1.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+        <link href='http://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:300,600' rel='stylesheet' type='text/css'>
+        <link href="assets/css/hover_pack.css" rel="stylesheet"> 
+        <link href="assets/css/main.css" rel="stylesheet"> 
 
         <style>
             .headtext
             {
-              color: #636363;
+              color: #545454;
               font-family: 'Open Sans', sans-serif;
             }
             .navbar-custom
@@ -45,30 +52,20 @@ $objectConvert = new Math(round($dataAvgSalaryFromAll["0"]),round($dataMaxSalary
               }
 
               .navbar-custom .navbar-nav > li > a
-              {
-                  color:#fff;
-              }
+              {  color:#fff; }
               .navbar-custom .navbar-nav > .active > a, .navbar-nav > .active > a:hover, .navbar-nav > .active > a:focus
                {
                   color: #ffffff;
                   background-color:transparent;
               }
               .navbar-custom .navbar-brand
-              {
-                  color:#eeeeee;
-              }
-            a
-            {
-              color: #6f5499;  
-            }
+              { color:#eeeeee; }           
             .brandText
-            {
-                
-            }
-            .brandText:hover
-            {
-                color: #636363;
-            }
+            {   }
+            .brandText:hover  {  color: #636363; }     
+                  
+   a { color: #654D8A; }   
+              a:hover { color: #654D8A; text-decoration: underline; }        
 
         </style>
 
@@ -77,6 +74,7 @@ $objectConvert = new Math(round($dataAvgSalaryFromAll["0"]),round($dataMaxSalary
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
     <script type="text/javascript" src="https://www.google.com/jsapi"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.0-beta.3/js/select2.min.js"></script>
+      <script src="assets/js/pace.js"></script> 
     <script type="text/javascript">
   $('select').select2();
 </script>
@@ -243,7 +241,7 @@ $objectConvert = new Math(round($dataAvgSalaryFromAll["0"]),round($dataMaxSalary
 
       var options = {
         title: "Группировка количества людей по зарплате",
-      
+         width: "100%",
         height: "720",
         bar: {groupWidth: "95%"},
         legend: { position: "none" },
@@ -264,14 +262,17 @@ $objectConvert = new Math(round($dataAvgSalaryFromAll["0"]),round($dataMaxSalary
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="#">SakhResumeAnalyst</a>
+      <a class="navbar-brand" href="/">SakhResumeAnalyst</a>
     </div>
 
 
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-        <li class="brandText"><a href="/">Общие диаграммы <span class="sr-only">(current)</span></a></li>
-         <li class="brandText"><a href="/">Диаграммы по типу деятельности</a></li>
+        <li class="brandText"><a href="#columnchart_values">Главная <span class="sr-only">(current)</span></a></li>
+        <li class="brandText"><a href="#columnchart_values_max_old">Максимальная зарплата по возрасту<span class="sr-only">(current)</span></a></li>
+        <li class="brandText"><a href="#columnchart_values1">Группировка по зарплате<span class="sr-only">(current)</span></a></li>    
+        <li class="brandText"><a href="#donutchartAvgType">Средняя зарплата по роду специализации</a> <span class="sr-only">(current)</span></a></li>
+        <li class="brandText"><a href="#donutchartMaxType">Максимальная зарплата по роду специализации</a> <span class="sr-only">(current)</span></a></li>
 
       </ul>
 
@@ -279,12 +280,36 @@ $objectConvert = new Math(round($dataAvgSalaryFromAll["0"]),round($dataMaxSalary
   </div>
 </nav>
 
-
+<div class="preloader"></div>
 <div class="page-header">
-  <center><h3 class="headtext">&nbsp;&nbsp;Аналитика резюме с сайта <a href="http://sakh.com">Sakh.com</a> по зарплатным критериям</h3></center><br>
-   <center> <h2 class="headtext"><u>Проанализировано <?php echo $dataCountResume[0]; ?> резюме</u></h2> </center>
+  <center><h3 class="headtext">&nbsp;&nbsp;Аналитика резюме с сайта <a href="http://sakh.com" >Sakh.com</a> по зарплатным критериям</h3></center><br><br>
+   <center> <h3 class="headtext">Проанализировано <?php echo $dataCountResume[0]; ?> резюме <?php echo "по состоянию на ".date("d.m.y"); ?></h3> </center>
 </div>
+ 
 <div class="container-fluid">
+       
+    <!--
+     <div class="container">
+     
+      <div class="row">
+        <div class="col-md-4">
+          <h2>Heading</h2>
+          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+        </div>
+        <div class="col-md-4">
+          <h2>Heading</h2>
+          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+       </div>
+        <div class="col-md-4">
+          <h2>Heading</h2>
+          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+        </div>
+      </div>
+    -->
+
 
 <!--
  <div class="row">    
@@ -335,6 +360,50 @@ $objectConvert = new Math(round($dataAvgSalaryFromAll["0"]),round($dataMaxSalary
 </div>
 </div>
 
+</div>
+    <div class="row">
+        <div class="col-md-12">
+            <center><p><a href="#columnchart_values"><h2><b>В начало</b></h2></a></p>	</center><br><br>
+            </div>
+        </div>
+<div id="footermain">
+		<div class="container-fluid">
+			<div class="row">
+				
+				<div class="col-lg-6 col-md-12 col-sm-12 col-xs-12 footerhop">
+					
+				<p><font class="headertext1"><span class="glyphicon glyphicon-th-list brandimg"></span>&nbsp;&nbsp;<b>Sakh.com Resume Analyst</b></font></p>
+                			
+		
+
+				</div>			
+			
+				<div class="col-lg-6 col-md-12 col-sm-12 col-xs-12 footerhop">
+					<center>
+				<p align="left"><font class="newfootertextbrandd"><u>Найти в социальных сетях </u></font></p>			
+				<p align="left"><a href="http://vk.com/id6139701"><font class="newfootertextbrandd">Вконтакте - Иванов Никита</font></a></p>
+				
+
+			</center>
+				</div>
+				
+				
+				<div class="col-lg-3">
+					
+				</div>
+				
+			
+				<div class="col-lg-3">
+					
+				</div>
+				
+				
+			</div>
+		</div>
+	</div>
+
+
+
 
 
 
@@ -354,7 +423,7 @@ $objectConvert = new Math(round($dataAvgSalaryFromAll["0"]),round($dataMaxSalary
         </div>
       </div>
 -->
-</div>
+
 
 </html>
 
