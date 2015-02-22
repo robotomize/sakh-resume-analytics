eb1b320778407e93708b1f1290c2b6816eb21ea0
Nikita <robotomize@gmail.com>
2015-02-22 20:47:21 +1000

    Update Model.php

--- Model.php
+++ Model.php
@@ -28,6 +28,19 @@ class Model
             $result = DBmodel::getInstance()->query("SELECT type, MAX(salary) FROM main_data GROUP BY type");
             return $result->fetchAll();
         }
+
+        public function getAvgSalaryVacancy()
+        {
+                $result = DBmodel::getInstance()->query("SELECT type, AVG(salary) FROM main_data_vacancy GROUP BY type");
+                return $result->fetchAll();
+        }
+
+        public function getMaxSalaryByTypeVacancy()
+        {
+            $result = DBmodel::getInstance()->query("SELECT type, MAX(salary) FROM main_data_vacancy GROUP BY type");
+            return $result->fetchAll();
+        }
+
         public function getMaxSalaryByOld()
         {
             $result = DBmodel::getInstance()->query("SELECT old, MAX(salary) FROM main_data GROUP BY old");
@@ -49,6 +62,18 @@ class Model
         {
            $result = DBmodel::getInstance()->query("SELECT MAX(salary) FROM main_data");
            return $result->fetch();
+        }
+
+        public function getAvgSalryFromAllVacancy()
+        {
+           $result = DBmodel::getInstance()->query("SELECT AVG(salary) from main_data_vacancy");
+           return $result->fetch(); 
+        }
+
+        public function getMaxSalryFromAllVacancy()
+        {
+           $result = DBmodel::getInstance()->query("SELECT Max(salary) from main_data_vacancy");
+           return $result->fetch(); 
         }
 
         public function getDataByTypeFromSalary()
@@ -75,9 +100,38 @@ class Model
            return $result->fetch(); 
         }
 
+        public function getCountVacancy()
+        {
+           $result = DBmodel::getInstance()->query("SELECT count(id) from main_data_vacancy");
+           return $result->fetch(); 
+        }   
+
+        public function getCheckEntryAboutVacancy()
+        {
+           $result = DBmodel::getInstance()->query("SELECT * from main_data where name=? and type=? ");
+           return $result->fetchAll();
+        }
+
         public function __construct()
         {
 
         }
 }
+
+$objectStart = new Model();
+
+$dataAvgSalaryByType = $objectStart->getAvgSalaryByOld();
+$dataMaxSalaryByOld = $objectStart->getMaxSalaryByOld();
+
+$dataAvgSalaryFromType = $objectStart->getAvgSalary();
+$dataMaxSalaryFromType = $objectStart->getMaxSalaryByType();
+
+$dataAvgSalaryFromAll = $objectStart->getAvgSalryFromAll();
+$dataMaxSalaryFromAll = $objectStart->getMaxSalryFromAll();
+$dataByTypeFromSalary = $objectStart->getDataByTypeFromSalary();
+$dataGroupSalaryByCount = $objectStart->getDataBySalaryCount();
+
+$dataCountResume = $objectStart->getCountResume();
+$objectConvert = new Math(round($dataAvgSalaryFromAll["0"]),round($dataMaxSalaryFromAll["0"]));
+
 
