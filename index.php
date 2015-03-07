f1d2e74943e0f03df5ec899c09caab5852baa429
Nikita <robotomize@gmail.com>
2015-03-07 17:24:25 +0400

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ require_once "model.php";
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Аналитика резюме с http://sakh.com</title>
+        <title>Аналитика резюме с sakh.com</title>
         <!--
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
@@ -73,18 +73,191 @@ require_once "model.php";
               } 
               .statisticInfo
               {
-
-                font-size: 4.3em;
-                color: #430C6B;
-
+                font-size: 3.8em;    
+                color: #404040;       
               }
               .statisticSmallInfo
               {
-                  font-size: 2.3em;
+                  font-size: 2.0em;
               } 
-        </style>
+              .salaryBox
+              { 
+                padding-top: 10px;
+                padding-bottom: 10px;
+                width: 35%;
+              border-color: #000;
+              border: 3px solid;          
+                -moz-border-radius: 5px;
+                -webkit-border-radius: 5px;
+                border-radius: 5px;
+                behavior: url(border-radius.htc); 
+                color: #404040;  
+              }
+              .salaryBox:hover
+              { 
+                padding-top: 10px;
+                padding-bottom: 10px;
+                width: 35%;
+              color: #fff; 
+              background-color: #404040;   
+              border-color: #000;
+              border: 3px solid;          
+                -moz-border-radius: 5px;
+                -webkit-border-radius: 5px;
+                border-radius: 5px;
+                behavior: url(border-radius.htc);   
+              }
+        </style>  
 
-    <script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
+  </head>
+<nav class="navbar navbar-custom">
+  <div class="container-fluid">
+
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="/">SakhResumeAnalyst</a>
+    </div>
+
+
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="brandText listM"><a href="#columnchart_values">Главная</a></li>
+        <li class="brandText listM"><a href="#columnchart_values_max_old">Максимальная зарплата по возрасту<span class="sr-only">(current)</span></a></li>        
+        <li class="brandText listM"><a href="#donutchartAvgType">Средняя зарплата по роду специализации</a> <span class="sr-only">(current)</span></a></li>
+        <li class="brandText listM"><a href="#donutchartMaxType">Максимальная зарплата по роду специализации</a> <span class="sr-only">(current)</span></a></li>
+
+      </ul>
+
+    </div>
+  </div>
+</nav>
+
+<div class="preloader"></div>
+<div class="page-header">
+  <center><h3 class="headtext">&nbsp;&nbsp;Аналитика резюме с сайта <a href="http://sakh.com" >Sakh.com</a> по зарплатным критериям</h3></center>
+ <!--  <center> <h3 class="headtext">Проанализировано <?php /*echo $dataCountResume[0]; */?> резюме <?php /* echo "по состоянию на ".date("d.m.y");*/ ?></h3> </center> -->
+</div>
+ 
+<div class="container-fluid">    
+   
+     
+      <div class="row">
+        
+        <div class="col-md-4">
+          <center><h2 class="statisticInfo salaryBox"> <?php echo Math::transformSalary($dataCountResume["0"]); ?></h3>
+          <small class="statisticSmallInfo">резюме проанализировано</small></center>
+        </div>
+        <div class="col-md-4">
+           <center><h2 class="statisticInfo salaryBox"> <?php echo Math::transformSalary($dataCountVacancy["0"]); ?></h3>
+          <small class="statisticSmallInfo">вакансий проанализировано</small></center>
+       </div>
+        <div class="col-md-4">
+         <center><h2 class="statisticInfo salaryBox"> <?php  echo $dataCountType["0"] ?></h3>
+          <small class="statisticSmallInfo">видов деятельности</small></center>
+          
+        </div>
+       <div>  
+       <div class="row">
+       </div>
+<br>
+  <br>
+    <br>
+
+      <div class="row">        
+        <div class="col-md-4">
+          <center><h2 class="statisticInfo salaryBox"> <?php echo Math::transformSalary(round($dataCountAvgSalaryFromResume["0"])); ?></h3>
+          <small class="statisticSmallInfo">Средняя зарплата по ожиданиям</small></center>
+        </div>
+        <div class="col-md-4">
+           <center><h2 class="statisticInfo salaryBox"> <?php echo Math::transformSalary(round($dataCountAvgSalaryFromVacancy["0"])); ?></h3>
+          <small class="statisticSmallInfo">Средняя зарплата по вакансиям</small></center>
+       </div>
+        <div class="col-md-4">         
+          
+        </div>
+
+       <div>   
+    
+
+  <div class="row">
+  <div class="col-md-12">
+   <div id="columnchart_values"></div>
+
+  </div>
+  </div>
+<div class="row">
+    <div class="col-md-12">
+   <div id="columnchart_values_max_old"></div>
+    </div>
+</div>
+
+<div class="row">
+<div class="col-md-12">
+<div id="columnchart_values1"></div>
+</div>
+</div>
+
+<div class="row">
+<div class="col-md-12">
+<div id="donutchartAvgType"></div>
+</div>
+</div>
+
+<div class="row">
+<div class="col-md-12">
+<div id="donutchartMaxType"></div>
+</div>
+</div>
+
+</div>
+    <div class="row">
+        <div class="col-md-12">
+            <center><p><a href="#columnchart_values"><h2><b>В начало</b></h2></a></p>	</center><br><br>
+            </div>
+        </div>
+<div id="footermain">
+		<div class="container-fluid">
+			<div class="row">
+				
+				<div class="col-lg-6 col-md-12 col-sm-12 col-xs-12 footerhop">
+					
+				<p><font class="headertext1"><span class="glyphicon glyphicon-th-list brandimg"></span>&nbsp;&nbsp;<b>Sakh.com Resume Analyst</b></font></p>
+                			
+		
+
+				</div>			
+			
+				<div class="col-lg-6 col-md-12 col-sm-12 col-xs-12 footerhop">
+					<center>
+				<p align="left"><font class="newfootertextbrandd"><u>Найти в социальных сетях </u></font></p>			
+				<p align="left"><a href="http://vk.com/id6139701"><font class="newfootertextbrandd">Вконтакте</font></a></p>
+				
+
+			</center>
+				</div>
+				
+				
+				<div class="col-lg-3">
+					
+				</div>
+				
+			
+				<div class="col-lg-3">
+					
+				</div>
+				
+				
+			</div>
+		</div>
+	</div>
+
+
+  <script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
     <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
     <script type="text/javascript" src="https://www.google.com/jsapi"></script>
@@ -104,9 +277,9 @@ require_once "model.php";
                 $counter = 0;
                 while($counter<count($dataAvgSalaryByType))
                 {
-                        $dataAvgSalaryByType[$counter][1] = round($dataAvgSalaryByType[$counter][1]);
+                        $dataAvgSalaryByType[$counter][1] =  round((round($dataAvgSalaryByType[$counter][1])+round($dataAvgSalaryByType[$counter+1][1]))/2);
                         echo "['{$dataAvgSalaryByType[$counter][0]}', {$dataAvgSalaryByType[$counter][1]},'stroke-color: #703593; stroke-width: 4; fill-color: #C5A5CF'],";
-                        $counter++;
+                        $counter = $counter + 2;
                 }
           ?>
         ]);
@@ -142,9 +315,9 @@ require_once "model.php";
                 $counter = 0;
                 while($counter<count($dataMaxSalaryByOld))
                 {
-                        $dataMaxSalaryByOld[$counter][1] = round($dataMaxSalaryByOld[$counter][1]);
+                        $dataMaxSalaryByOld[$counter][1] = round((round($dataMaxSalaryByOld[$counter][1])+round($dataMaxSalaryByOld[$counter+1][1]))/2);
                         echo "['{$dataMaxSalaryByOld[$counter][0]}', {$dataMaxSalaryByOld[$counter][1]},'stroke-color: #871B47; stroke-opacity: 0.8; stroke-width: 8; fill-color: #BC5679; fill-opacity: 0.2'],";
-                        $counter++;
+                        $counter = $counter +2;
                 }
           ?>
         ]);
@@ -265,180 +438,6 @@ require_once "model.php";
       chart.draw(view, options);
   }
   </script>
-
-  </head>
-<nav class="navbar navbar-custom">
-  <div class="container-fluid">
-
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="/">SakhResumeAnalyst</a>
-    </div>
-
-
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav">
-        <li class="brandText listM"><a href="#columnchart_values">Главная</a></li>
-        <li class="brandText listM"><a href="#columnchart_values_max_old">Максимальная зарплата по возрасту<span class="sr-only">(current)</span></a></li>
-        <li class="brandText listM"><a href="#columnchart_values1">Группировка по зарплате<span class="sr-only">(current)</span></a></li>    
-        <li class="brandText listM"><a href="#donutchartAvgType">Средняя зарплата по роду специализации</a> <span class="sr-only">(current)</span></a></li>
-        <li class="brandText listM"><a href="#donutchartMaxType">Максимальная зарплата по роду специализации</a> <span class="sr-only">(current)</span></a></li>
-
-      </ul>
-
-    </div>
-  </div>
-</nav>
-
-<div class="preloader"></div>
-<div class="page-header">
-  <center><h3 class="headtext">&nbsp;&nbsp;Аналитика резюме с сайта <a href="http://sakh.com" >Sakh.com</a> по зарплатным критериям</h3></center>
- <!--  <center> <h3 class="headtext">Проанализировано <?php /*echo $dataCountResume[0]; */?> резюме <?php /* echo "по состоянию на ".date("d.m.y");*/ ?></h3> </center> -->
-</div>
- 
-<div class="container-fluid">    
-   
-     
-      <div class="row">
-        
-        <div class="col-md-4">
-          <center><h2 class="statisticInfo"> <?php echo $dataCountResume["0"]; ?></h3>
-          <small class="statisticSmallInfo">резюме проанализировано</small></center>
-        </div>
-        <div class="col-md-4">
-           <center><h2 class="statisticInfo"> <?php echo $dataCountVacancy["0"]; ?></h3>
-          <small class="statisticSmallInfo">вакансий проанализировано</small></center>
-       </div>
-        <div class="col-md-4">
-          <h2>Heading</h2>
-          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-        </div>
-
-       <div>
-
-        
-   
-   
-
-
-<!--
- <div class="row">    
-   <div class="col-md-3"><center><h4>Выберите ваш тип специализации</h4>
-    <select class="js-example-basic-single col-md-12">
-    <?php
-    /*
-                $counter = 0;
-                while($counter<count($dataByTypeFromSalary))
-                {
-                        echo "<option value=AL>".$dataByTypeFromSalary[$counter]["0"]."</option>";
-                        $counter++;
-                }
-                */
-    ?>
-    </select></center>
-    </div>
-  </div>
--->
-
-  <div class="row">
-  <div class="col-md-12">
-   <div id="columnchart_values"></div>
-
-  </div>
-  </div>
-<div class="row">
-    <div class="col-md-12">
-   <div id="columnchart_values_max_old"></div>
-    </div>
-</div>
-
-<div class="row">
-<div class="col-md-12">
-<div id="columnchart_values1"></div>
-</div>
-</div>
-
-<div class="row">
-<div class="col-md-12">
-<div id="donutchartAvgType"></div>
-</div>
-</div>
-
-<div class="row">
-<div class="col-md-12">
-<div id="donutchartMaxType"></div>
-</div>
-</div>
-
-</div>
-    <div class="row">
-        <div class="col-md-12">
-            <center><p><a href="#columnchart_values"><h2><b>В начало</b></h2></a></p>	</center><br><br>
-            </div>
-        </div>
-<div id="footermain">
-		<div class="container-fluid">
-			<div class="row">
-				
-				<div class="col-lg-6 col-md-12 col-sm-12 col-xs-12 footerhop">
-					
-				<p><font class="headertext1"><span class="glyphicon glyphicon-th-list brandimg"></span>&nbsp;&nbsp;<b>Sakh.com Resume Analyst</b></font></p>
-                			
-		
-
-				</div>			
-			
-				<div class="col-lg-6 col-md-12 col-sm-12 col-xs-12 footerhop">
-					<center>
-				<p align="left"><font class="newfootertextbrandd"><u>Найти в социальных сетях </u></font></p>			
-				<p align="left"><a href="http://vk.com/id6139701"><font class="newfootertextbrandd">Вконтакте - Иванов Никита</font></a></p>
-				
-
-			</center>
-				</div>
-				
-				
-				<div class="col-lg-3">
-					
-				</div>
-				
-			
-				<div class="col-lg-3">
-					
-				</div>
-				
-				
-			</div>
-		</div>
-	</div>
-
-
-
-
-
-
-<!--
-       <div><strong>Зарплата относительно средней(<?php /* echo round($dataAvgSalaryFromAll["0"]); */?>)</strong></div><br>
-    <div class="progress">
-      <div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="25000" aria-valuemin="0" aria-valuemax=<?php /*echo round($dataAvgSalaryFromAll["0"]); */?> style=<?php /*echo $objectConvert->ProgressBarAvg(); */?>>
-
-      </div>
-    </div> -->
-    <!-- на будущее прогресс бары -->
-<!--
-<div><strong>Зарплата относительно максимальной(<?php /* echo round($dataMaxSalaryFromAll["0"]); */?>)</strong></div><br>
-    <div class="progress">
-        <div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="25000" aria-valuemin="0" aria-valuemax=<?php /*echo round($dataMaxSalaryFromAll["0"]); */?> style=<?php /*echo $objectConvert->ProgressBarMax(); */?>>
-
-        </div>
-      </div>
--->
 
 
 </html>
