28484c1c952bdd8c7d16ce15df7a4e2e4fd8d309
Nikita <robotomize@gmail.com>
2015-03-07 17:23:33 +0400

    Update Math.php

--- Math.php
+++ Math.php
@@ -14,12 +14,16 @@ class Math
 	{
 		return round(round(35000/self::$maxValue)*100);
 	}
-
+	public static function transformSalary($salary)
+	{
+	   return round(($salary / 1000))." ".($salary % 1000);
+	 } 
 	 public function __construct($param1,$param2)
         {
         	self::$avgValue = $param1;
         	self::$maxValue = $param2;
         }
+        
 
 
 }
